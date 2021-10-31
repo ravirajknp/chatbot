c7645a470bf0f463858eedde628eb84bb5c58b5f
Ravi Kumar <raj.ravi.me@gmail.com>
2021-11-01 00:45:54 +0530

    chatbotpushtogit

---
+++ index.js
@@ -0,0 +1,6 @@
+const toggle = () => {
+  const bot = document.querySelector("#bot");
+  const botButton  = document.querySelector("#botButton");
+  bot?.classList?.toggle("hide");
+  botButton?.classList?.toggle("hide");
+}
